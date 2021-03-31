69fb0363e826257027c6bf9f1c84e984594fba68
alitaso345 <alice.maru345@gmail.com>
2021-04-01 00:42:17 +0900

    Add getTweetsByList

--- function.go
+++ function.go
@@ -34,42 +34,11 @@ func HTTPFunction(_w http.ResponseWriter, _r *http.Request) {
 		}
 	}
 
-	consumerKey := os.Getenv("CONSUMER_KEY")
-	consumerSecretKey := os.Getenv("CONSUMER_SECRET_KEY")
-	accessToken := os.Getenv("ACCESS_TOKEN")
-	accessTokenSecret := os.Getenv("ACCESS_TOKEN_SECRET")
-
-	config := oauth1.NewConfig(consumerKey, consumerSecretKey)
-	token := oauth1.NewToken(accessToken, accessTokenSecret)
-	httpClient := config.Client(oauth1.NoContext, token)
-
-	client := twitter.NewClient(httpClient)
-	members, resp, err := client.Lists.Members(&twitter.ListsMembersParams{ListID: 1375784169129738240, Count: 1000})
-	if err != nil {
-		log.Fatalf("Error getting members %v", err)
-	}
-
+	tweets := getTweetsByList(1375784169129738240)
 	var text string
-	if resp.StatusCode == 200 {
-		for _, user := range members.Users {
-			tweets, resp, err := client.Timelines.UserTimeline(&twitter.UserTimelineParams{
-				UserID:          user.ID,
-				TrimUser:        twitter.Bool(false),
-				ExcludeReplies:  twitter.Bool(true),
-				IncludeRetweets: twitter.Bool(false),
-			})
-			if err != nil {
-				log.Fatalf("Error getting statuses %v", err)
-			}
-			if resp.StatusCode == 200 {
-				for _, tweet := range tweets {
-					if containNoticeKeyword(tweet.Text) && !containIgnoreKeyword(tweet.Text) && isRecentTweet(tweet) {
-						text = text + "https://twitter.com/" + tweet.User.ScreenName + "/status/" + strconv.FormatInt(tweet.ID, 10) + "\n\n"
-
-					}
-				}
-			}
-
+	for _, tweet := range tweets {
+		if containNoticeKeyword(tweet.Text) && !containIgnoreKeyword(tweet.Text) && isRecentTweet(tweet) {
+			text = text + "https://twitter.com/" + tweet.User.ScreenName + "/status/" + strconv.FormatInt(tweet.ID, 10) + "\n\n"
 		}
 	}
 
@@ -142,6 +111,42 @@ func LineBotWebhookFunction(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	fmt.Fprintf(w, "ok")
+}
+
+func getTweetsByList(listId int64) []twitter.Tweet {
+	consumerKey := os.Getenv("CONSUMER_KEY")
+	consumerSecretKey := os.Getenv("CONSUMER_SECRET_KEY")
+	accessToken := os.Getenv("ACCESS_TOKEN")
+	accessTokenSecret := os.Getenv("ACCESS_TOKEN_SECRET")
+
+	config := oauth1.NewConfig(consumerKey, consumerSecretKey)
+	token := oauth1.NewToken(accessToken, accessTokenSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	client := twitter.NewClient(httpClient)
+	members, resp, err := client.Lists.Members(&twitter.ListsMembersParams{ListID: listId, Count: 1000})
+	if err != nil {
+		log.Fatalf("Error getting members %v", err)
+	}
+
+	var response []twitter.Tweet
+	if resp.StatusCode == 200 {
+		for _, user := range members.Users {
+			tweets, resp, err := client.Timelines.UserTimeline(&twitter.UserTimelineParams{
+				UserID:          user.ID,
+				TrimUser:        twitter.Bool(false),
+				ExcludeReplies:  twitter.Bool(true),
+				IncludeRetweets: twitter.Bool(false),
+			})
+			if err != nil {
+				log.Fatalf("Error getting statuses %v", err)
+			}
+			if resp.StatusCode == 200 {
+				response = append(response, tweets...)
+			}
+		}
+	}
+	return response
 }
 
 func getUserIds() []string {
